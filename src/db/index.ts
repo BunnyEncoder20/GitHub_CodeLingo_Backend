cfdc7d1633c58bc2ce5902faa57c7c1e9457b0f7
Bunny <varun.verma2020@vitstudent.ac.in>
2024-08-20 21:44:29 +0530

    Created seperate file for connecting to db

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+import { DB_name } from "@/constants.js";
+import { log } from "console";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
+        // console.log(connectionInstance);
+        console.log(`MongoDB connection successful !!! \nDB Host : ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.error("[db] MongoDB connection failed. Aborting...");
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express, { Application } from "express";
+import connectDB from "./db/index.js";
+
 import { DB_name } from "./constants.js";
 
 const app: Application = express();
@@ -8,17 +10,7 @@ dotenv.config({
 	path: "./.env",
 });
 
-(async () => {
-	try {
-		await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`);
-		console.log("Connected to MongoDB");
 
-		// App listening to requests
-		app.listen(process.env.PORT, () => {
-			console.log("Server is listening on port : ", process.env.PORT);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-})();
+// Connecting to the db
+connectDB()
+
