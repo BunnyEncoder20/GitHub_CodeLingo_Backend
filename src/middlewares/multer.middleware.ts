4841d71798a38c31488922ac3048b085dabaeda6
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-09-23 17:45:20 +0530

    fixed multer file upload bug

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,17 +1,19 @@
 import multer, { StorageEngine } from "multer";
 import { Request } from "express";
+import path from "path";
 // import { v4 as uuidv4 } from "uuid";  // Use for generating unique IDs
 
 // Define the storage with type safety
 const storage: StorageEngine = multer.diskStorage({
     destination: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void): void {
-        cb(null, '../../public/temp');
+        const temp_dir = path.join(__dirname, "../../public/temp");
+        cb(null, temp_dir);
     },
     filename: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void): void {
         const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1E9);
         cb(null, `${file.fieldname}_${uniqueSuffix}`);
-        console.log(file);
-        console.log(`📁 Received ${file.originalname} for upload`);
+        console.log("[middlware] ",file);
+        console.log(`[middlware] 📁 Received ${file.originalname} for upload`);
     }
 });
 
